bb231904804f314d1c06abd178939cb3b168aaeb
Andri Óskarsson <account+github@andri.dk>
2024-01-14 09:54:11 +0100

    wip

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const showDefaultLang = false;
+
+export const languages = {
+  en: "English",
+  dk: "Dansk",
+};
+
+export const defaultLang = "dk";
+
+export const ui = {
+  en: {
+    "nav.host": "Host",
+    "nav.join_long": "Join our next Hackathon",
+    "nav.join_short": "Join",
+  },
+  dk: {
+    "nav.host": "Bliv vært",
+    "nav.join_long": "Deltag i vores næste hackathon",
+    "nav.join_short": "Deltag",
+  },
+} as const;
